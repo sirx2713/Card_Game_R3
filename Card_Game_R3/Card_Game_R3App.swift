59b79e1256d1c2e4f5c9d77507a477f0dc4abd31
Mabende <belindamabende@gmail.com>
2024-05-24 14:39:53 +0200

    Initial Commit

---
+++ Card_Game_R3/Card_Game_R3App.swift
@@ -0,0 +1,17 @@
+//
+//  Card_Game_R3App.swift
+//  Card_Game_R3
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Card_Game_R3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card_Game_R3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Card_Game_R3
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
